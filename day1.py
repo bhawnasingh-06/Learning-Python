39882666c15ebcd855d3b83e79e16ae1844c4c90
Bhawna Singh <68336130+bhawnasingh-06@users.noreply.github.com>
2022-04-02 23:11:07 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,25 @@
+#Writing first statement in python
+print("Hello bhai :)\nAur batao kese ho?")
+
+
+#indentation is very important in python which means leaving a TAB space from margin and in python it is used to show that statements which are indented belong to upper statement.
+if (5>4): 
+    print("5 is greater than 4") 
+    print("Got it yeah!")
+
+else: 
+    print("Bye guys bye!")  
+
+'''This is multi line comment!'''
+
+
+#Here we have imported a built-in module 'math'
+
+
+import math 
+a = 2
+b = 4
+c = math.sqrt(16)
+d = math.pow(a,b)
+print(c)
+print(d)
