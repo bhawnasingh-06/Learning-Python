ba9968bbeaed8ed4dc1d08e672f16efc883553e5
Bhawna Singh <68336130+bhawnasingh-06@users.noreply.github.com>
2022-04-03 23:29:21 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,35 @@
+print("Type Casting")
+
+a = 52
+b = 58.68
+c = "Bhawna"
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+a = 15
+b = "25"
+print(type(b))
+b = int(b)
+print(type(b) )
+print("Sum of a and b is",a+b)
+
+
+#Strings
+a = "Hello"
+print(a)
+
+
+#Strings slicing
+a = "Hello"
+print(a[0])
+print(a[1])
+print(a[2:5])
+
+
+print(a.strip())
+#This function allows us to remove all the blank spaces near to string.
+
+print(len(a))
+#len function counts the total characters of any string.
